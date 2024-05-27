725db319bcfb9b4c385817ce6e690838119d1274
shrwn08 <shrwnkumar08@gmail.com>
2024-05-28 01:58:43 +0530

    pushing Header

---
+++ qtify-spotify-clone/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header"
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ qtify-spotify-clone/src/Components/Header.js
@@ -0,0 +1,79 @@
+
+    import React from "react";
+    import { Box, } from "@mui/material";
+    import Qtify from "./Assets/Qtify-removebg-preview.png";
+    // import Headphone from "./headphone.png";
+    import SearchIcon from "@mui/icons-material/Search"
+    import './Header.css'
+    function Header() {
+      return (
+        <Box>
+          <Box
+            sx={{
+              height: "74px",
+              backgroundColor: "#34c94b",
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box color="White" fontSize="25px" fontWeight="900">
+              <img
+                src={Qtify}
+                alt="logo"
+                style={{ backgroundColor: "transparent",
+                width:"67px",
+                height:"34px",
+                marginLeft:"32px" }}
+              />
+            </Box>
+    
+            <Box className= "search-container">
+            <input type="text" className="search-input" placeholder="Search a song of your choice" />
+            <button className="search-button">
+            <SearchIcon  height="20px" width="20px"/>
+          </button>
+            </Box>
+            <button className="feedbackBtn">
+              Give Feedback
+            </button>
+          </Box>
+          {/* <Box
+            sx={{
+              height: "270px",
+              width:"100%",
+              backgroundColor: "#121212",
+              display:"flex",
+              justifyContent:"center",
+              alignItems:"center"
+            }}
+          >
+            <Box
+              sx={{
+                width: "799px",
+                height: "212px",
+                display:"flex",
+                alignItems:"center"
+              }}
+            >
+              <Box
+                sx={{
+                  color: "#ffff",
+                  fontSize: "36px",
+                    textAlign:"center"
+                }}
+              >
+                100 Thousand songs, ad free Over thousand podcast episodes
+              </Box>
+              <Box>
+                <img src={Headphone} alt="headphone" width="150px" height="150px" />
+              </Box>
+            </Box>
+          </Box> */}
+        </Box>
+      );
+    }
+    
+    export default Header;
+ 
